f53f4803bca3bbaa4a8330e34023df5134470819
MrJeffLin <104509747+MrJeffLin@users.noreply.github.com>
2023-01-16 22:30:57 -0500

    Create task.dart

---
+++ task.dart
@@ -0,0 +1,13 @@
+class task {
+  String name;
+  String description;
+  String todoDate;
+  String todoTime;
+  int itemNum;
+  task(
+      {this.name = "",
+      this.description = "",
+      this.todoDate = "",
+      this.todoTime = "",
+      this.itemNum = 0});
+}
